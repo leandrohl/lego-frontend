4ef11b3b6d8828e6d919f31be370f456cd12bbb7
Leandro Silva <leandro.silva.ext@vr.com.br>
2024-05-16 18:53:19 -0300

    feat: implementando footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+
+
+import './styles.scss'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p> LEGO, the LEGO logo and the Brick and Knob configurations are trademarks of the LEGO Group. ©2022 The LEGO Group. </p>
+        </footer>
+    )   
+}
+
+export default Footer;
